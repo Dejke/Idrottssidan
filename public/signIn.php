1305b8723e0ea8e424da775226d70b38d4256306
kasperdejke <kasper.dejke.student@ksgyf.se>
2018-04-19 23:46:01 +0200

    memeade signin.php lite

--- public/signIn.php
+++ public/signIn.php
@@ -6,14 +6,17 @@
 	<style type="text/css">
 		
 		#email-warning{
-			display: none;
+			
+            display: none;
 			color: red;
 		}
 		#password-warning{
-			display: none;
+			
+            display: none;
 			color: red;
 		}
         .signup-fields{
+            
             display:none;
         }
 
@@ -24,43 +27,56 @@
 <body>
 
 	<!-- Eller ska den skicka till self?!?!? hmmm idk meme -->
-	<form action="db_add_user.php" method="post" onsubmit="return validate(this)">
+	<form method="post" onsubmit="return validate(this)">
 		
 		<span id="email-warning">Email måste matcha<br></span>
 		<label>Email</label><br>
 		<input type="text" name="email"><br>
 
         <div class="signup-fields">
-            <label>Email igen</label><br>
+            <label>Upprepa email</label><br>
             <input type="text" name="email2"><br><br>
         </div>
 		
-        <span id="password-warning" >Lösenord måste matcha<br></span>
+        <span id="password-warning">Lösenord måste matcha<br></span>
 		<span>Lösenord</span><br>
 		<input type="password" name="password"><br>
 
         <div class="signup-fields">
-            <label>Lösenord igen</label><br>
+            <label>Upprepa lösenord</label><br>
             <input type="password" name="password2"><br><br>
 
-            <label>Klass</label><br>
+            <label>Klass</label>
             <select name="class">
                 <?
                     /*hämta klasser typ*/
                 ?>
-            </select><br>
+            </select><br><br>
         </div>
-		<input type="submit" value="Logga in">
+		<div class="login-fields">
+            <input formaction="db_login.php" type="submit" value="Logga in">
+        </div>
+        
+        <div class="signup-fields">
+            <input formaction="db_add_user.php" type="submit" value="Registrera">
+        </div>    
         <br>
-        <a href="#" onclick="showSignupFields();">Registrera ett konto</a>
 
 	</form>
-    
+    <div class="login-fields">
+        <a href="#" id="signup">Registrera ett konto</button>
+    </div>
+    <div class="signup-fields">
+        <a href="#" id="signin">Logga in</button>
+    </a>
 </body>
 <script type="text/javascript">
-		
-    function validate(form){
+	var currentForm = "signin";
 
+    function validate(form){
+        if(currentForm == "signin"){
+            return true;
+        }
         var e = form.elements;
         var passwordMatch = false;
         var emailMatch = false;
@@ -72,7 +88,6 @@
 
         } else {
             document.getElementById('email-warning').style.display = "inline";
-
         }
 
         if (e["password"].value == e["password2"].value){
@@ -88,8 +103,18 @@
         return passwordMatch && emailMatch;
 
     }
-    //Lägg till jquery listener...
 
+    $("#signup").click(function(){
+        $(".signup-fields").css("display", "block");
+        $(".login-fields").css("display", "none");
+        currentForm = "signup";
+    });
+
+    $("#signin").click(function(){
+        $(".signup-fields").css("display", "none");
+        $(".login-fields").css("display", "block");
+        currentForm = "signup";
+    });
 </script>
 
 </html>
