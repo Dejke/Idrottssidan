4aab968adf6b834293a35cb14b2e8ff33e462014
Sebastian Johansson <sebbe.johansson@live.se>
2018-04-25 14:41:43 +0200

    Meme

--- public/db_add_user.php
+++ public/db_add_user.php
@@ -1,22 +1,54 @@
 <?
 
-	include "secret.php";
-	$mysqli = new mysqli($SECRET["url"], $SECRET["user"], $SECRET["password"], $SECRET["database"]);
-    
-    if($mysqli->connect_errno){
-        echo "something is wrong my dude".$mysqli->connect_error;
-        exit;
-    }
-
-    $mysqli->set_charset("utf8");
-
+	include "sql_setup.php";
 
     if ($_POST["email"] && $_POST["password"]){
 
+    	if ($stmt = $mysqli->prepare("
 
-    	echo "it be working";
-    	$ay = password_hash($_POST["password"], PASSWORD_BCRYPT);
-    	echo strlen($ay);
+    		SELECT EMAIL
+    		FROM USERS
+    		WHERE EMAIL = ?
+
+    	")){
+
+    		$stmt->bind_param("s", $email);
+
+   			$email = $_POST["email"];
+
+   			$stmt->execute();
+   			if ($stmt->fetch()){
+   				header("Location: signIn.php?message=existingemail");
+   			}
+
+   			$stmt->close();
+
+    	}
+
+    	if ($stmt = $mysqli->prepare("
+
+    		INSERT INTO USERS (EMAIL, PASSWORD, FIRST_NAME, LAST_NAME, PROGRAMME, GRADE, LETTER)
+    		VALUES (?, ?, ?, ?, ?, ?, ?)
+
+    	")){
+
+    		$stmt->bind_param("sssssis", $email, $password, $first_name, $last_name, $programme, $grade, $letter);
+
+    		$email = $_POST["email"];
+    		$password = password_hash($_POST["password"], PASSWORD_BCRYPT);
+    		$first_name = $_POST["fname"];
+    		$last_name = $_POST["lname"];
+    		$programme = $_POST["programme"];
+    		$grade = $_POST["grade"];
+    		$letter = $_POST["letter"];
+
+    		$stmt->execute();
+    		$stmt->close();
+
+    		
+   			header("Location: signIn.php?message=accountcreated");
+
+    	}
 
 
     } else echo "sorry fam";
