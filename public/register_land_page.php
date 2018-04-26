5a63f0580fd47d3021c82f439db40ef4b8e3c1af
kasperdejke <kasper.dejke.student@ksgyf.se>
2018-04-26 08:47:20 +0200

    yes

---
+++ public/register_land_page.php
@@ -0,0 +1,9 @@
+<html>
+    <head>
+        <title>Registrera konto</title>
+    </head>
+
+    <body>
+        
+    </body>
+</html>
--- public/signIn.php
+++ public/signIn.php
@@ -85,7 +85,7 @@
 		<input type="text" name="email" pattern="[a-zA-Z0-9_.]+@?ksgyf.se" title="Ange en emailadress från ksgyf."><br>
 
         <label>Upprepa ksgyf-email</label><br>
-        <input type="text" name="email2"<br><br>
+        <input type="text" name="email2"><br><br>
 
 
         <span id="password-warning"><br></span>
