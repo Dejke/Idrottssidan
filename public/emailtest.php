e3ce68e2911d1062d06e6a95ca9fcc5915d6343b
Sebastian Johansson <sebbe.johansson@live.se>
2018-05-07 07:58:08 +0200

    AJAJAJAJA OOOF SRRT BIDIBAPBAP BOP

---
+++ public/emailtest.php
@@ -0,0 +1,67 @@
+<?
+	
+	$address = "martin.persson@ksgyf.se";
+	
+	// Get the adress from a form.
+	/*
+	$address = test_input($_POST["email"]);
+	if (!filter_var($address, FILTER_VALIDATE_EMAIL)) {
+  		$emailErr = "Invalid email format"; 
+	}
+	*/
+	
+////////////////////////MAIL-SCRIPT////////////////////////////////////////////
+	//ini_set('display_errors', 1);
+	//error_reporting(~0);
+
+	require_once('phpmailer/class.phpmailer.php');
+	//include("phpmailer/class.smtp.php"); // optional, gets called from within class.phpmailer.php if not already loaded
+	$mail             = new PHPMailer();
+
+
+	$body 				= "Välkommen till svampklubben!<br><br>
+							På onsdag ska vi börja våra studier av svampar i hemmet.<br>
+							Vi börjar hemma hos Bosse som säger att det växer en massa intressanta saker på hans vind.
+							Tiden är 18:00 - 20:00.<br><br>
+							Torleif står för fikat den här veckan.<br><br>
+							Mvh Sigurd Murkla";
+	
+	$mail->IsSMTP(); // telling the class to use SMTP
+	$mail->Host       = "mailcluster.loopia.se"; // SMTP server
+	$mail->SMTPDebug  = 0;                     // enables SMTP debug information (for testing)
+											   // 1 = errors and messages
+											   // 2 = messages only
+	$mail->SMTPAuth   = true;                  // enable SMTP authentication
+	$mail->SMTPSecure = "tls";                 // sets the prefix to the server
+	$mail->Port		  = 587;
+	$mail->Username   = "noreply@kaggteknik.se";  // username
+	$mail->Password   = "bumbibjorn3000";          // password
+
+	//$mail->SetFrom('noreply@kaggteknik.se', 'Lasse Kagg');
+	//$mail->AddReplyTo("noreply@kaggteknik.se","Lasse Kagg");
+	
+	$mail->SetFrom('sigurd@svampklubben.se', utf8_decode('Ordföranden'));
+	$mail->AddReplyTo("noreply@svampklubben.se","svampklubben");
+	$mail->Subject    = utf8_decode("Svampklubbens möte");
+	//$mail->AltBody    = "To view the message, please use an HTML compatible email viewer!"; // optional, comment out and test
+	$mail->MsgHTML($body);
+
+	// *********** ADRESSER ATT SKICKA TILL !! *********
+	//$address = $email;
+	$mail->AddAddress($address);
+	//$mail->AddCC("KopiaTill@adress", "Namn på personen");
+	//$mail->AddAttachment("images/phpmailer.gif");      // attachment
+	//$mail->AddAttachment("images/phpmailer_mini.gif"); // attachment
+
+
+	// *********** KODEN HÄR UNDER SKICKAR IVÄG DITT MAIL **********
+	/*
+	if(!$mail->Send()) {
+	  echo "Mailer Error: " . $mail->ErrorInfo;
+	} else {
+	  //header('Location: index.php');
+	  echo "Your mail has been sent!";
+	}
+	*/
+	
+?>
---
+++ public/phpmailer/mailscript.php
@@ -0,0 +1,54 @@
+﻿<?php
+
+	//ini_set('display_errors', 1);
+	//error_reporting(~0);
+
+	require_once('phpmailer/class.phpmailer.php');
+	//include("phpmailer/class.smtp.php"); // optional, gets called from within class.phpmailer.php if not already loaded
+
+	$mail             = new PHPMailer();
+
+	//$body             = file_get_contents('contents.html');
+	//$body             = eregi_replace("[\]",'',$body);
+
+	$body 				= "
+	Här är ditt lösenord: ".$password."<br>"."
+	Var snäll och byt lösenord när du loggar in.
+							";
+	
+	$mail->IsSMTP(); // telling the class to use SMTP
+	$mail->Host       = "mail.scorpionshops.com"; // SMTP server
+	$mail->SMTPDebug  = 2;                     // enables SMTP debug information (for testing)
+											   // 1 = errors and messages
+											   // 2 = messages only
+	$mail->SMTPAuth   = true;                  // enable SMTP authentication
+	$mail->SMTPSecure = "tls";                 // sets the prefix to the server
+	$mail->Port		  = 25;
+	$mail->Username   = "kaggnist@kaggteknik.se";  // username
+	$mail->Password   = "bumbibjorn3000";          // password
+
+	$mail->SetFrom('olofenstrom.kaggteknik.se', 'Lasse Kagg');
+	$mail->AddReplyTo("no-reply@olofenstrom.kaggteknik.se","Lasse Kagg");
+	$mail->Subject    = "Aktivering av konto för aktivitetsdagar";
+	//$mail->AltBody    = "To view the message, please use an HTML compatible email viewer!"; // optional, comment out and test
+	$mail->MsgHTML($body);
+
+	// *********** ADRESSER ATT SKICKA TILL !! *********
+	$address = "någonsadress@någondomän.se";
+	$mail->AddAddress($address, "Namn på personen");
+	//$mail->AddCC("KopiaTill@adress", "Namn på personen");
+	//$mail->AddAttachment("images/phpmailer.gif");      // attachment
+	//$mail->AddAttachment("images/phpmailer_mini.gif"); // attachment
+	
+	echo $address.'<br><br>';
+	echo $body.'<br>';
+
+	// *********** KODEN HÄR UNDER SKICKAR IVÄG DITT MAIL **********
+	/*if(!$mail->Send()) {
+	  echo "Mailer Error: " . $mail->ErrorInfo;
+	} else {
+	  echo "Message sent!";
+	}
+	*/
+?>
+
