0493acf2bec0161b697f5391396fa89999258170
kasperdejke <kasper.dejke.student@ksgyf.se>
2018-05-25 12:53:09 +0200

    yes

--- public/db_password_reset.php
+++ public/db_password_reset.php
@@ -1,4 +1,11 @@
 <?
+    include "secret.php";
+    require_once('phpmailer/class.phpmailer.php');
+    
+
+    error_reporting(E_ALL);
+    ini_set('display_errors', 'on');
+    echo "MEME";
     include "sql_setup.php";
     /*
         $_POST[”email"] vilket konto som ska återställas
@@ -11,7 +18,7 @@
 
     if ($stmt = $mysqli->prepare("
 
-        SELECT ID
+    		SELECT ID
     		FROM USERS
     		WHERE EMAIL = ?
 
@@ -27,8 +34,6 @@
             //An account is associated with the email
 
             //Remove any existing PASSWORD_RESETS entries
-            echo "Error 1 :".$mysqli->error;
-            $stmt = null;
             if($stmt = $mysqli->prepare("
                 DELETE FROM PASSWORD_RESETS
                     WHERE USER_ID = ?
@@ -46,22 +51,46 @@
                         VALUES
                             (?,?)
                 ")){
-
+                    if (!filter_var($address, FILTER_VALIDATE_EMAIL)) {
+                          header("Location: signIn.php?message=invalidemail");
+                    }
                     $key = uniqid('',true);
                     $stmt->bind_param("si", $key, $id);
                     $stmt->execute();
-                    header("Location:register_land_page.php");
-                    exit;
+
+                    /* 
+                        SEND AN EMAIL
+                    */
+                    $mail             = new PHPMailer();
+
+
+                    $mail->IsSMTP(); // telling the class to use SMTP
+                    $mail->Host       = "mailcluster.loopia.se"; // SMTP server
+                    $mail->SMTPDebug  = 0;                     // enables SMTP debug information (for testing)
+                    $mail->SMTPAuth   = true;                  // enable SMTP authentication
+                    $mail->SMTPSecure = "tls";                 // sets the prefix to the server
+                    $mail->Port		  = 587;
+                    $mail->Username   = "noreply@kaggteknik.se";  // username
+                    $mail->Password   = $SECRET["mail_password"]; 
+
+	                $mail->SetFrom('noreply@kaggteknik.se', 'Lasse Kagg');
+	                $mail->AddReplyTo("noreply@kaggteknik.se","Lasse Kagg");
+                    $mail->MsgHTML($_SERVER["HTTP_HOST"]."/password_change.php?str=".$key);
+                    $mail->AddAddress($_POST["email"]);
+
+                    if(!$mail->Send()) {
+                        echo "Mailer Error: " . $mail->ErrorInfo;
+                    } else {
+                        header("Location:register_land_page.php");
+                        exit;
+                    }
                 }
             }
         }
         else{
             //email is ZUCC 
-            header("Location: signIn.php?message=invalidemail");
+            header("Location: signIn.php?message=noexistemail");
             exit;
         }
-    }
-    else{
-        echo "shits fucked 1";
-    }
+    }  
 ?>
