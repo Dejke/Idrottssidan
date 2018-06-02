dada6caa0b07bd1b6dc1d8e1ba1d3f3e5fd57873
Sebastian Johansson <sebbe.johansson@live.se>
2018-06-02 15:09:21 +0200

    please work now

--- public/user_page.php
+++ public/user_page.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?
     session_start();
 
@@ -24,31 +23,4 @@
 	hihihaha
 
 </body>
-=======
-<?
-    session_start();
-
-    if(!isset($_SESSION["USER"])){
-        header("Location: signIn.php");
-        exit;
-    }
-    
-    //We be testing, yo chiidslllaisdsddowkawdasdawddwdjadwadsdiow*/
-    include "sql_setup.php";
-?>
-<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-	<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
-	<link rel="stylesheet" type="text/css" href="stylesheet/main.css">
-</head>
-<body>
-
-	<?include "header.php";?>
-
-	hihihaha
-
-</body>
->>>>>>> 0f17326623a4b437097701079dcae2498b99f028
 </html>
